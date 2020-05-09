c776d62c87cfc2bf4d1b75d223d4c56370fbdd29
brngdsn <bgoodson@hocus-pocus.local>
2020-05-09 17:37:43 -0400

    patched devops docs

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ```curl -o- https://raw.githubusercontent.com/brngdsn/eg-cloud-host-a/master/devops/install.sh | bash```
 
+# machine config
+
+execute the `devops/config.sh` script to route incoming 80->8000 and 443->8443
--- devops/config.sh
+++ devops/config.sh
@@ -6,5 +6,8 @@ sudo iptables -t nat -L
 # reroute incoming packets on 80 to 8000
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000
 
+# hmm..
+# sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443
+
 # serve current directory (e.g., `cd dist/ && serve`)
 # serve --port 8000
