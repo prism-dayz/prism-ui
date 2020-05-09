4fb406a344b96a9389be79bbeb8ea7ff458ec605
brngdsn <bgoodson@hocus-pocus.local>
2020-05-09 17:20:20 -0400

    added source

---
+++ INFO.md
@@ -0,0 +1,3 @@
+https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/TroubleshootingInstancesConnecting.html#troubleshoot-unprotected-key
+
+chmod 0400 .ssh/my_private_key.pem
---
+++ devops/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# this file aquires binary dependencies for the build run-time
+
+# update this machine
+sudo yum update -y
+
+# add git
+sudo yum install -y git
+
+# should get zsh here
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+
+# source it
+. ~/.nvm/nvm.sh
+
+# install nodejs
+nvm install v12.16.2
+
+# install yarn
+npm install -g yarn
