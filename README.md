0967a245256ba1b1dacba451214b12701c9c5e9e
brngdsn <bgoodson@hocus-pocus.local>
2020-05-09 17:33:20 -0400

    patched with devops and routing

---
+++ README.md
@@ -0,0 +1,4 @@
+# machine provision
+
+```curl -o- https://raw.githubusercontent.com/brngdsn/eg-cloud-host-a/master/devops/install.sh | bash```
+
---
+++ devops/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# check the routes
+sudo iptables -t nat -L
+
+# reroute incoming packets on 80 to 8000
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8000
+
+# serve current directory (e.g., `cd dist/ && serve`)
+# serve --port 8000
